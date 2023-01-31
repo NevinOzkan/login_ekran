65aa9b83bdf5305535c842ae9c8c6fe561835532
NevinOzkan <ozkannevin73@gmail.com>
2023-01-31 19:05:28 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/myfirstpro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myfirstpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranYuksekligi = ekranBilgisi.size.height;
+    final double ekranGenisligi = ekranBilgisi.size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: ekranYuksekligi / 20),
+              child: SizedBox(
+                width: ekranGenisligi / 3,
+                height: ekranYuksekligi / 6,
+                child: Image.asset("Images/log_in.jpg"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(ekranYuksekligi / 30),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Kullanıcı Adı",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(ekranYuksekligi / 30),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Şifre",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(ekranYuksekligi / 30),
+              child: SizedBox(
+                width: ekranGenisligi / 1.2,
+                height: ekranYuksekligi / 12,
+                child: ElevatedButton(
+                  child: Text(
+                    "GİRİŞ YAP",
+                    style: TextStyle(fontSize: ekranGenisligi / 25),
+                  ),
+                  onPressed: () {
+                    print("Giriş Yapıldı");
+                  },
+                ),
+              ),
+            ),
+            Text(
+              "Yardım ?",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
